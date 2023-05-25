12370f8d7229a15ed661d30155c30cc5738370d6
DavidFrenchSG <David.French@gov.scot>
2023-05-25 21:24:09 +0100

    Working on, but not necessarily improving, slide outputs

--- CN_analysis_build.R
+++ CN_analysis_build.R
@@ -6,8 +6,8 @@ cropyear_range <- (2019:2021)
 #Specify the farmtypes to be included, and their order. 1: Cereal, 2: General cropping, 3: Dairy,
 #4: LFA Sheep, 5: LFA Cattle, 6: LFA Cattle & Sheep, 7: Lowland livestock, 8: Mixed
 #9: All farm types, 10: All LFA livestock types
-# Output_types = c(9,1,2,3,10,8)
-Output_types=c(1)
+Output_types = c(9,1,2,3,10,8)
+# Output_types=c(1)
 
 #Use cropyears to determine sampyears
 sampyear_range <- cropyear_range + 1
@@ -19,6 +19,7 @@ financial_years <- paste0(financial_years_start,"-",financial_years_end)
 #Specify the document title
 title_line1 <- paste0("Farm Business Survey ",max(financial_years),":")
 title_line2 <- "Farm level emissions and nitrogen usage"
+title_single_line <- paste0(title_line1," ", title_line2)
 
 
 
@@ -27,3 +28,5 @@ source('CN_analysis.R')
 source('Plots.R')
 rmarkdown::render('CN_analysis_md.Rmd')
 rmarkdown::render('CN_slides.Rmd')
+rmarkdown::render('CN_ppt.Rmd')
+
